7323f57ecb9213434fde571ef468b778ede3c657
Emanuele Biotti <emanuelebiotti@localhost.localdomain>
2019-12-17 10:46:19 +0100

    aggiunta componente About con react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, {Component} from 'react';
 import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
 import Header from './components/layout/header';
+import About from './components/pages/About'
 import uuid from 'uuid';
 
 class App extends Component {
@@ -63,13 +65,20 @@ class App extends Component {
   render() {
     console.log(this.state.todos);
     return (
-      <div className="App">
-        <div className="container">
-          <Header/>
-          <AddTodo addTodo={this.addTodo}/>
-          <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header/>
+            <Route exact path="/" render={props => (
+              <React.Fragment>
+                <AddTodo addTodo={this.addTodo}/>
+                <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
+              </React.Fragment>
+            )}/>
+            <Route path="/about" component={About}/>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
